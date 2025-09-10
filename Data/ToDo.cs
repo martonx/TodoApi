4b15c67553cdaf6569ea730e63f169ad6bf41500
Marton Lajos <lajosmarton@hotmail.com>
2025-09-10 07:53:32 +0200

    Database context WIP

--- Data/ToDo.cs
+++ Data/ToDo.cs
@@ -3,6 +3,9 @@
 public class ToDo
 {
     public int Id { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime Deadline { get; set; }
+    public DateTime Created { get; set; }
+    public bool IsReady { get; set; }
 }
---
+++ Data/ToDoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class ToDoDbContext : DbContext
+{
+    public DbSet<ToDo> ToDos { get; set; }
+
+    public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+            : base(options)
+    {
+    }
+}
