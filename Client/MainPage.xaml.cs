af8be56ddfcd22631614d45740e595a07823c99f
Marton Lajos <lajosmarton@hotmail.com>
2025-09-17 09:06:27 +0200

    UI listázás

--- Client/MainPage.xaml.cs
+++ Client/MainPage.xaml.cs
@@ -8,30 +8,21 @@ namespace Client
     {
         private readonly IHttpClientFactory httpClientFactory;
         private ObservableCollection<ToDoDto> toDoCollection = [];
-        int count = 0;
 
         public MainPage(IHttpClientFactory httpClientFactory)
         {
             this.httpClientFactory = httpClientFactory;
             InitializeComponent();
-        }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            ToDosView.ItemsSource = toDoCollection;
         }
 
         private void OnWeatherClicked(object? sender, EventArgs e)
         {
             var httpClient = httpClientFactory.CreateClient();
             var toDos = httpClient.GetFromJsonAsync<List<ToDoDto>>("https://localhost:7241/list").Result;
+
+            toDoCollection.Clear();
 
             foreach (var toDo in toDos)
                 toDoCollection.Add(toDo);
