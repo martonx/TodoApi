55382aef59a41859809375c3447b50f1e5728596
Marton Lajos <lajosmarton@hotmail.com>
2025-10-15 22:39:24 +0200

    Identity started

--- Data/ToDoDbContext.cs
+++ Data/ToDoDbContext.cs
@@ -1,8 +1,33 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace Data;
 
-public class ToDoDbContext(DbContextOptions<ToDoDbContext> options) : DbContext(options)
+public class ToDoDbContext(DbContextOptions<ToDoDbContext> options) : IdentityDbContext(options)
 {
     public DbSet<ToDo> ToDos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+
+        optionsBuilder.UseAsyncSeeding(async (context, _, cancellationToken) =>
+        {
+            await InsertRolesAsync(context, cancellationToken);
+        });
+    }
+
+    private async Task InsertRolesAsync(DbContext context, CancellationToken cancellationToken)
+    {
+        var roles = context.Set<IdentityRole>();
+        if (!roles.Any(role => role.Name == "Admin"))
+        {
+            roles.Add(new IdentityRole { Name = "Admin" });
+            roles.Add(new IdentityRole { Name = "User" });
+
+            await context.SaveChangesAsync(cancellationToken);
+        }
+    }
 }
--- TODO/Program.cs
+++ TODO/Program.cs
@@ -1,18 +1,35 @@
 ﻿using Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.AddSecurity("JWT", new NSwag.OpenApiSecurityScheme
+    {
+        Type = NSwag.OpenApiSecuritySchemeType.ApiKey,
+        Name = "Authorization",
+        In = NSwag.OpenApiSecurityApiKeyLocation.Header,
+        Description = "Type 'Bearer {your JWT token}' into the field below."
+    });
+
+    config.OperationProcessors.Add(new NSwag.Generation.Processors.Security.AspNetCoreOperationSecurityScopeProcessor("JWT"));
+});
+
+//builder.Services.AddAuthorizationBuilder()
+//  .AddPolicy("admin", policy => policy.RequireRole("Admin"))
+//  .AddPolicy("user", policy => policy.RequireRole("User"));
 
 // Configure the Database connection
 var connectionString = builder.Configuration.GetConnectionString("ToDoDbContext");
 builder.Services.AddDbContext<ToDoDbContext>(options =>
   options.UseSqlServer(connectionString));
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ToDoDbContext>();
 
 // Configure the DI container
 builder.Services.AddTransient<IToDoService, ToDoService>();
@@ -29,6 +46,8 @@ builder.Services.AddCors(options =>
                       });
 });
 
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 app.UseCors(allowSpecificOrigins);
@@ -36,40 +55,61 @@ app.UseCors(allowSpecificOrigins);
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseOpenApi();
+    app.UseSwaggerUi();
 }
 
-app.MapGet("get/{id:int}", async (int id, IToDoService service) =>
+app.MapGroup("Account").WithTags("Account").MapIdentityApi<IdentityUser>();
+
+app.MapGroup("ToDo").WithTags("ToDo").MapGet("get/{id:int}", async (int id, IToDoService service) =>
 {
     return await service.GetAsync(id);
-});
+}).RequireAuthorization();
 
-app.MapGet("list", async (
-    [FromQuery(Name = "isReady")] bool? isReady,
+app.MapGroup("ToDo").WithTags("ToDo").MapGet("list", async (
+    [FromQuery(Name = "isReady")] bool? isReady, HttpContext httpContext,
     IToDoService service) =>
 {
     return await service.ListAllAsync(isReady);
-});
+}).RequireAuthorization();
 
-app.MapPost("create", async (ToDo model, IToDoService service) =>
+app.MapGroup("ToDo").WithTags("ToDo").MapPost("create", async (ToDo model, IToDoService service) =>
 {
     await service.CreateAsync(model);
 
     return Results.Created();
-});
+}).RequireAuthorization();
 
-app.MapPut("update", async (ToDo model, IToDoService service) =>
+app.MapGroup("ToDo").WithTags("ToDo").MapPut("update", async (ToDo model, IToDoService service) =>
 {
     await service.UpdateAsync(model);
 
     return Results.Ok();
-});
+}).RequireAuthorization();
 
-app.MapDelete("delete/{id:int}", async (int id, IToDoService service) =>
+app.MapGroup("ToDo").WithTags("ToDo").MapDelete("delete/{id:int}", async (int id, IToDoService service) =>
 {
     await service.DeleteAsync(id);
 
     return Results.Ok();
-});
+}).RequireAuthorization();
+
+//Auto migration
+using var serviceScope = (app as IApplicationBuilder).ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
+using var dbContext = serviceScope.ServiceProvider.GetService<ToDoDbContext>();
+var migrations = await dbContext.Database.GetPendingMigrationsAsync();
+if (migrations.Any())
+{
+    await dbContext.Database.MigrateAsync();
+}
+
+var roles = dbContext.Set<IdentityRole>();
+if (!await roles.AnyAsync(role => role.Name == "Admin"))
+{
+    roles.Add(new IdentityRole { Name = "Admin" });
+    roles.Add(new IdentityRole { Name = "User" });
+
+    await dbContext.SaveChangesAsync();
+}
 
 app.Run();
