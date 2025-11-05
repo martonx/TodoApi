48f5449be577096c49b7a3deb2c67c0eccfe6065
Marton Lajos <lajosmarton@hotmail.com>
2025-11-05 22:33:05 +0100

    Api automated tests first works

--- Api.Tests/Api.Tests/Tests.cs
+++ Api.Tests/Api.Tests/Tests.cs
@@ -5,33 +5,32 @@ using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
 
-namespace Api.Tests
+namespace Api.Tests;
+
+public class Tests
 {
-    public class Tests
+    [ClassDataSource<WebApplicationFactory>(Shared = SharedType.PerTestSession)]
+    public required WebApplicationFactory WebApplicationFactory { get; init; }
+
+    public HttpClient client;
+
+    [Before(Test)]
+    public async Task Setup(TestContext context, CancellationToken cancellationToken)
     {
-        [ClassDataSource<WebApplicationFactory>(Shared = SharedType.PerTestSession)]
-        public required WebApplicationFactory WebApplicationFactory { get; init; }
+        client = WebApplicationFactory.CreateClient();
+        var response = await client.PostAsJsonAsync("/account/login",
+            new LoginRequest { Email = "lajosmarton@hotmail.com", Password = "Lali1978*" });
+        var responseString = await response.Content.ReadAsStringAsync();
+        var jwtResponse = JsonSerializer.Deserialize<AccessTokenResponse>(responseString,
+            new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtResponse.AccessToken);
+    }
 
-        private HttpClient client;
+    [Test]
+    public async Task TestListing()
+    {
+        var toDos = await client.GetFromJsonAsync<List<ToDoDto>>("/todo/list");
 
-        [Before(Test)]
-        public async Task Setup(TestContext context, CancellationToken cancellationToken)
-        {
-            client = WebApplicationFactory.CreateClient();
-            var response = await client.PostAsJsonAsync("/account/login", 
-                new LoginRequest { Email = "lajosmarton@hotmail.com", Password = "Lali1978*" });
-            var responseString = await response.Content.ReadAsStringAsync();
-            var jwtResponse = JsonSerializer.Deserialize<AccessTokenResponse>(responseString,
-                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtResponse.AccessToken);
-        }
-
-        [Test]
-        public async Task TestListing()
-        {
-            var toDos = await client.GetFromJsonAsync<List<ToDoDto>>("/todo/list");
-
-            await Assert.That(toDos).IsNotEmpty();
-        }
+        await Assert.That(toDos).IsNotEmpty();
     }
 }
