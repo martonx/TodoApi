5484aa63d79ab259cf553d50be2df045f2b57684
Marton Lajos <lajosmarton@hotmail.com>
2025-11-12 22:59:34 +0100

    test cleanup

--- Api.Tests/Api.Tests/GlobalSetup.cs
+++ Api.Tests/Api.Tests/GlobalSetup.cs
@@ -1,4 +1,42 @@
 ﻿// Here you could define global logic that would affect all tests
 
 // You can use attributes at the assembly level to apply to all tests in the assembly
+using Api.Tests;
+
+using Data;
+
+using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.EntityFrameworkCore;
+
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+
 [assembly: System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+
+public class GlobalApi
+{
+    [ClassDataSource<WebApplicationFactory>(Shared = SharedType.PerTestSession)]
+    public static WebApplicationFactory WebApplicationFactory { get; set; }
+
+    public static HttpClient Client;
+
+    [Before(TestSession)]
+    public static async Task SetupApiClient()
+    {
+        Client = WebApplicationFactory.CreateClient();
+        var response = await Client.PostAsJsonAsync("/account/login",
+            new LoginRequest { Email = "lajosmarton@hotmail.com", Password = "Password1." });
+        var responseString = await response.Content.ReadAsStringAsync();
+        var jwtResponse = JsonSerializer.Deserialize<AccessTokenResponse>(responseString,
+            new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtResponse.AccessToken);
+    }
+
+    [After(TestSession)]
+    public static void CleanUp()
+    {
+        Client.Dispose();
+    }
+}
--- Api.Tests/Api.Tests/Tests.cs
+++ Api.Tests/Api.Tests/Tests.cs
@@ -1,35 +1,14 @@
 ﻿using Common;
-using Microsoft.AspNetCore.Authentication.BearerToken;
-using Microsoft.AspNetCore.Identity.Data;
-using System.Net.Http.Headers;
 using System.Net.Http.Json;
-using System.Text.Json;
 
 namespace Api.Tests;
 
 public class Tests
 {
-    [ClassDataSource<WebApplicationFactory>(Shared = SharedType.PerTestSession)]
-    public required WebApplicationFactory WebApplicationFactory { get; init; }
-
-    public HttpClient client;
-
-    [Before(Test)]
-    public async Task Setup(TestContext context, CancellationToken cancellationToken)
-    {
-        client = WebApplicationFactory.CreateClient();
-        var response = await client.PostAsJsonAsync("/account/login",
-            new LoginRequest { Email = "lajosmarton@hotmail.com", Password = "Password1." });
-        var responseString = await response.Content.ReadAsStringAsync();
-        var jwtResponse = JsonSerializer.Deserialize<AccessTokenResponse>(responseString,
-            new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtResponse.AccessToken);
-    }
-
     [Test]
     public async Task TestListing()
     {
-        var toDos = await client.GetFromJsonAsync<List<ToDoDto>>("/todo/list");
+        var toDos = await GlobalApi.Client.GetFromJsonAsync<List<ToDoDto>>("/todo/list");
 
         await Assert.That(toDos).IsNotEmpty();
     }
